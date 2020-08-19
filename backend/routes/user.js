b21d141db932839a1c1fb3c5344c4c8b563ded5e
Sadeu Michel <michel.sadeu@yahoo.fr>
2020-08-19 21:17:49 +0200

    authentification pour éviter les attaques par force brute

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,9 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const userCtrl = require('../controllers/user');
+const ExpressBrute = require('express-brute');
+const store = new ExpressBrute.MemoryStore();
+const bruteforce = new ExpressBrute(store);
 
 //Implémentation des routeurs pour l'authentification de l'utilisateur
 router.post('/signup', userCtrl.signup);
-router.post('/login', userCtrl.login);
+router.post('/login', bruteforce.prevent, userCtrl.login);
 
 module.exports = router;
